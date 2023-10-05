4b449e7810891da98942398bcbb0a1675b22d7b8
Engineerie <jacob.bertilsson@gmail.com>
2023-10-05 22:17:48 +0100

    added app: baseURL: '/skogssvamp/' buildAssetsDir:'assets'

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import { resolve } from "path";
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/skogssvamp/', // baseURL: '/<repository>/'
+    buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
+  },
   target: 'static',
   router: {
     base: '/skogssvamp/'
