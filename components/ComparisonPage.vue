61720b7dd5b1f1ed2926900db015f2df912f3ecd
Engineerie <jacob.bertilsson@gmail.com>
2023-10-09 15:01:11 +0100

    applied styling to comparison view

--- components/ComparisonPage.vue
+++ components/ComparisonPage.vue
@@ -1,24 +1,60 @@
 <!-- ComparisonPage.vue -->
 <template>
-    <div class="comparison-grid">
-      <div v-for="env in environments" :key="env.query">
-        <div>
-        Query: {{ env.query }}
-      </div>
-      <div>
-        Red Listed Message: {{ env.RedListedMessage }}
-      </div>
-      <div>
-        Environment Message: {{ env.EnvironmentMessage }}
-      </div>
-      </div>
+    <div class="grid grid-cols-5 gap-8">
+        <div v-for="env in environments" :key="env.query">
+            <div class=" gap-2 mb-4">
+                <BaseTag shape="rounded" color="info" flavor="pastel" class="m-1">{{ env.query.geography.split(" ")[0] }}</BaseTag>
+                <BaseTag shape="rounded" color="success" flavor="pastel" class="m-1">{{ env.query.forestType }}</BaseTag>
+                <BaseTag shape="rounded" color="warning" flavor="pastel" class="m-1">{{ env.query.vegetationType }}</BaseTag>
+                <BaseTag shape="rounded" color="danger" flavor="pastel" class="m-1">{{ env.query.standAge.split(" ")[0] }}</BaseTag>
+             </div>
+                
+             <BaseCard shape="curved" class="p-6 mb-4">
+                <BaseHeading
+                as="h4"
+                size="sm"
+                weight="medium"
+                lead="tight"
+                class="mb-2 text-muted-400"
+              >
+              Environment Info: 
+              </BaseHeading>
+              <BaseParagraph size="md" lead="tight" color="primary">
+                {{ env.EnvironmentMessage }}
+              </BaseParagraph>
+            </BaseCard>
+
+            <BaseCard shape="curved" class="p-6" color="primary">
+                <BaseHeading
+                  as="h4"
+                  size="sm"
+                  weight="medium"
+                  lead="tight"
+                  class="mb-2 "
+                >
+                Redlisted Species 
+                </BaseHeading>
+                <BaseHeading weight="semibold" size="4xl" lead="tight">
+                    {{ env.RedListedMessage }}
+                </BaseHeading>
+              </BaseCard>
+
+              
+
+        </div>  
     </div>
   </template>
   
   <script setup>
   import { useComparisonStore } from '~/stores/comparisonStore';
+  import { useTitleStore } from '~/stores/titleStore';
+  import { onMounted } from 'vue';
 
-  
+  const titleStore = useTitleStore();
+
+  onMounted(() => {
+    titleStore.setTitle('Compare Environments');
+    });
 
   const comparisonStore = useComparisonStore();
   const environments = comparisonStore.environments;
@@ -26,10 +62,5 @@
   console.log("Fetched environments: ", environments);  // Debugging line
   </script>
   
-  <style>
-  .comparison-grid {
-    display: grid;
-    grid-template-columns: repeat(5, 1fr);
-  }
-  </style>
+ 
   
