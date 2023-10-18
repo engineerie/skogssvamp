7ecd6682dad1a87061f882465559f258adf94dda
Engineerie <jacob.bertilsson@gmail.com>
2023-10-18 08:55:52 +0200

    temporary changed root dir to skogssvamp

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 import { resolve } from "path";
 export default defineNuxtConfig({
   app: {
-    // baseURL: '/skogssvamp/', // baseURL: '/<repository>/'
+    baseURL: '/skogssvamp/', // baseURL: '/<repository>/'
     // buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
   },
   target: 'static',
