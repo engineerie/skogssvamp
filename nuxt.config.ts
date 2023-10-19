8c772c18ab7006be0909810d1e183f649ae2d451
Engineerie <jacob.bertilsson@gmail.com>
2023-10-19 09:49:19 +0200

    commented out target static

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,10 @@
 import { resolve } from "path";
 export default defineNuxtConfig({
   app: {
-    baseURL: '/skogssvamp/', // baseURL: '/<repository>/'
-    buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
+    // baseURL: '/skogssvamp/', // baseURL: '/<repository>/'
+    // buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
   },
-  target: 'static',
+  // target: 'static',
   modules: [
     'nuxt-icons',
     '@pinia/nuxt',
